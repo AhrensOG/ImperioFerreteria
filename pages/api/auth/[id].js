b758e310f82880ff3c4cc6b9fe27ace4b49ebbf3
AhrensOG <ahrensgabriel09@gmail.com>
2023-11-29 22:08:36 -0300

    Add PUT method to User controller

--- pages/api/auth/[id].js
+++ pages/api/auth/[id].js
@@ -18,5 +18,30 @@ export default async function handler(req, res) {
     } catch (error) {
       return res.status(400).json(error.message)
     };
-  };
+  } else if (req.method === 'PUT') {
+    try {
+      const { id } = req.query;
+      const { phone, address } = req.body;
+      if(!id) {
+        return res.status(400).json('An ID is required');
+      };
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json('User not found');
+      };
+      if (phone) {
+        await user.update({ phone });
+      };
+      if (address) {
+        await user.update({ address });
+      };
+
+      const updated = await User.findOne({ where: { id }, include: { model: Cart } });
+
+      return res.status(200).send(updated);
+
+    } catch (error) {
+      return res.status(400).json(error.message);
+    }
+  }
 };
