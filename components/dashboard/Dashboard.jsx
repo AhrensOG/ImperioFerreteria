9e4b5a7047482c3ab96366dc76771869eb64b5c9
Ivixx09 <estudio.xx23@gmail.com>
2023-12-07 10:46:31 -0300

    User detail in dashboard finished

--- components/dashboard/Dashboard.jsx
+++ components/dashboard/Dashboard.jsx
@@ -3,37 +3,46 @@ import SideBar from "./auxiliarComponents/SideBar";
 import User from "./user/User";
 import Products from "./products/Products";
 import { Context } from "@/context/GlobalContext";
-import { getAllProducts } from "@/context/actions";
+import { getAllProducts, getAllUsers } from "@/context/actions";
 
 const Dashboard = () => {
-  const [showProducts, setShowProducts] = useState(false);
-  const [showCategories, setShowCategories] = useState(false);
-  const [showUsers, setShowUsers] = useState(false);
+    const [showProducts, setShowProducts] = useState(false);
+    const [showCategories, setShowCategories] = useState(false);
+    const [showUsers, setShowUsers] = useState(false);
 
-  const { state, dispatch } = useContext(Context)
+    const { state, dispatch } = useContext(Context);
 
-  useEffect(() => {
-    getAllProducts(dispatch)
-  }, []);
+    useEffect(() => {
+        getAllProducts(dispatch);
+        getAllUsers(dispatch);
+    }, []);
 
-  return (
-    <div className="flex flex-row">
-      <div className="">
-        <SideBar
-          setShowProducts={setShowProducts}
-          showProducts={showProducts}
-          setShowCategories={setShowCategories}
-          showCategories={showCategories}
-          setShowUsers={setShowUsers}
-          showUsers={showUsers}
-        />
-      </div>
-      <div className="">
-        {showUsers ? <User /> : <div className="hidden"></div>}
-        {showProducts ? <Products products={state.products} /> : <div className="hidden"></div>}
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-row">
+            <div className="">
+                <SideBar
+                    setShowProducts={setShowProducts}
+                    showProducts={showProducts}
+                    setShowCategories={setShowCategories}
+                    showCategories={showCategories}
+                    setShowUsers={setShowUsers}
+                    showUsers={showUsers}
+                />
+            </div>
+            <div className="flex justify-center w-full">
+                {showUsers ? (
+                    <User users={state.users} />
+                ) : (
+                    <div className="hidden"></div>
+                )}
+                {showProducts ? (
+                    <Products products={state.products} />
+                ) : (
+                    <div className="hidden"></div>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Dashboard;
