bbed0ee7ea5b6e1fc13b0afce706038f10885cbd
AhrensOG <ahrensgabriel09@gmail.com>
2023-12-25 16:40:34 -0300

    Add soft delete to products model | Add payOrder button and its action | Change controllers to show deleted products in order section of admin dashboard

--- pages/api/products/controller.js
+++ pages/api/products/controller.js
@@ -1,122 +1,138 @@
-import { Categories, Order, Products, ProductsImages } from "@/db/models/models"
-
+import {
+  Categories,
+  Order,
+  Products,
+  ProductsImages,
+} from "@/db/models/models";
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     try {
       const { productId } = req.query;
-      
-      if( productId ) {
-        const product = await Products.findOne({ where: { id: productId }, include: [
-          { model: ProductsImages },
-          { model: Order }
-        ] })
 
-        if ( !product ) {
-          return res.status(400).send('Product doesnt exists')
+      if (productId) {
+        const product = await Products.findOne({
+          where: { id: productId },
+          include: [{ model: ProductsImages }, { model: Order }],
+          paranoid: false,
+        });
+
+        if (!product) {
+          return res.status(400).send("Product doesnt exists");
         }
 
-        return res.status(200).send(product)
-      };
+        return res.status(200).send(product);
+      }
 
-      const products = await Products.findAll({ include: [
-        { model: ProductsImages },
-        { model: Order },
-        { model: Categories }
-      ] });
+      const products = await Products.findAll({
+        include: [
+          { model: ProductsImages },
+          { model: Order },
+          { model: Categories },
+        ],
+        paranoid: false,
+      });
 
-      return res.status(200).send(products)
+      return res.status(200).send(products);
     } catch (error) {
-      return res.status(400).send(error)
+      return res.status(400).send(error);
     }
-  } else if (req.method === 'POST') {
+  } else if (req.method === "POST") {
     try {
       const { title, firstImage, description, price, quantity } = req.body;
 
-      if ( !title || !firstImage || !description || !price || !quantity ) {
-        return res.status(200).send('Missing Data')
-      };
+      if (!title || !firstImage || !description || !price || !quantity) {
+        return res.status(200).send("Missing Data");
+      }
 
       const foundProduct = await Products.findOne({ where: { title } });
 
       if (foundProduct) {
-        return res.status(200).send('Already exists a product with same name')
-      };
+        return res.status(200).send("Already exists a product with same name");
+      }
 
       await Products.create({
         title,
         firstImage,
         description,
         price,
-        quantity
-      })
+        quantity,
+      });
 
-      const newProduct = await Products.findOne({ where: { title }, include: [
-        { model: ProductsImages },
-        { model: Order },
-        { model: Categories }
-      ] })
+      const newProduct = await Products.findOne({
+        where: { title },
+        include: [
+          { model: ProductsImages },
+          { model: Order },
+          { model: Categories },
+        ],
+        paranoid: false
+      });
 
-      return res.status(200).send(newProduct)
-
+      return res.status(200).send(newProduct);
     } catch (error) {
-      return res.status(400).send(error.message)
+      return res.status(400).send(error.message);
     }
-  } else if (req.method === 'PUT') {
+  } else if (req.method === "PUT") {
     try {
-      const { title, firstImage, description, price, quantity, productId } = req.body;
+      const { title, firstImage, description, price, quantity, productId } =
+        req.body;
 
       const product = await Products.findByPk(productId);
 
-      if ( !product ) {
-        return res.status(200).send('Product doesnt exists');
-      };
+      if (!product) {
+        return res.status(200).send("Product doesnt exists");
+      }
 
-      if ( title ) {
+      if (title) {
         await product.update({ title });
-      };
-      if ( firstImage ) {
+      }
+      if (firstImage) {
         await product.update({ firstImage });
-      };
-      if ( description ) {
+      }
+      if (description) {
         await product.update({ description });
-      };
-      if ( price ) {
+      }
+      if (price) {
         await product.update({ price });
-      };
-      if ( quantity ) {
+      }
+      if (quantity) {
         await product.update({ quantity });
-      };
+      }
 
-      const updated = await Products.findOne({ where: { id: productId }, include: [
-        { model: ProductsImages },
-        { model: Order },
-        { model: Categories }
-      ] })
+      const updated = await Products.findOne({
+        where: { id: productId },
+        include: [
+          { model: ProductsImages },
+          { model: Order },
+          { model: Categories },
+        ],
+        paranoid: false
+      });
 
-      return res.status(200).send(updated)
-
+      return res.status(200).send(updated);
     } catch (error) {
-      return res.status(400).send(error)
+      return res.status(400).send(error);
     }
-  } else if (req.method === 'DELETE') {
+  } else if (req.method === "DELETE") {
     try {
       const { productId } = req.query;
 
       const product = await Products.findByPk(productId);
 
-      if ( !product ) {
-        return res.status(200).send('Product doesnt exists');
+      if (!product) {
+        return res.status(200).send("Product doesnt exists");
       }
 
       await product.destroy();
 
       const deleted = await Products.findByPk(productId);
 
-      return deleted ? res.status(400).send('Error deleting product') : res.status(200).send('Deleted successfully')
-
+      return deleted
+        ? res.status(400).send("Error deleting product")
+        : res.status(200).send("Deleted successfully");
     } catch (error) {
-      return res.status(400).send(error)
+      return res.status(400).send(error);
     }
-  };
-};
+  }
+}
